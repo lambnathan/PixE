f6cc90348baf980fecb39a307f5a90ef48861ba8
nlambe <nlambert@mymail.mines.edu>
2020-03-07 12:43:45 -0700

    generate button now only shows when the user has chosen all the required elements

--- app/src/main/java/com/imageaccident/pixe/MainActivity.kt
+++ app/src/main/java/com/imageaccident/pixe/MainActivity.kt
@@ -3,11 +3,13 @@ package com.imageaccident.pixe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,6 +22,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private lateinit var generateButton : Button
     private lateinit var algorithmButton: Button
     private lateinit var orientationButton: Button
+
+    private var hasChosenPicture: Boolean = false
+    private var hasChosenAlgorithm: Boolean = false
+    private var hasChosenOrientation: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,26 +47,78 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         toggle.isDrawerIndicatorEnabled = true
 
-        takePhotoButton.setOnClickListener{Toast.makeText(baseContext, "User will be directed to Camera app to take photo", Toast.LENGTH_SHORT).show()}
-        choosePictureButton.setOnClickListener{Toast.makeText(baseContext, "User will be directed to Photo Gallery app to choose picture", Toast.LENGTH_SHORT).show()}
+        takePhotoButton.setOnClickListener{Toast.makeText(baseContext, "User will be directed to Camera app to take photo", Toast.LENGTH_SHORT).show()
+            hasChosenPicture = true
+            checkGenerateButton()
+        }
+        choosePictureButton.setOnClickListener{Toast.makeText(baseContext, "User will be directed to Photo Gallery app to choose picture", Toast.LENGTH_SHORT).show()
+            hasChosenPicture = true
+            checkGenerateButton()
+        }
         generateButton.setOnClickListener{
             Toast.makeText(baseContext, "New image will be generated!", Toast.LENGTH_SHORT).show()
             // booleans to make this work or not
         }
 
+        generateButton.visibility = View.INVISIBLE
+
+
         algorithmButton.setOnClickListener{ showAlgoMenu(algorithmButton)}
         orientationButton.setOnClickListener { showOrientationMenu(orientationButton)}
+    }
+
+    private fun checkGenerateButton(){
+        if(hasChosenAlgorithm && hasChosenOrientation && hasChosenPicture){
+            generateButton.visibility = View.VISIBLE
+        }
     }
 
     private fun showAlgoMenu(button: Button){
         val popupMenu = PopupMenu(this, button)
         popupMenu.menuInflater.inflate(R.menu.algorithm_popup_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener {item ->
+            when(item.itemId){
+                R.id.sort_rgb ->{
+                    Toast.makeText(this, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                    hasChosenAlgorithm = true
+                    checkGenerateButton()
+                }
+                R.id.sort_hue -> {
+                    Toast.makeText(this, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                    hasChosenAlgorithm = true
+                    checkGenerateButton()
+                }
+                R.id.sort_gamma -> {
+                    Toast.makeText(this, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                    hasChosenAlgorithm = true
+                    checkGenerateButton()
+                }
+            }
+            true
+
+        }
         popupMenu.show()
     }
 
     private fun showOrientationMenu(button: Button){
         val popupMenu = PopupMenu(this, button)
         popupMenu.menuInflater.inflate(R.menu.orientation_pop_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener {item ->
+            when(item.itemId){
+                R.id.orientation_horizontal ->{
+                    Toast.makeText(this, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                    hasChosenOrientation = true
+                    checkGenerateButton()
+                }
+                R.id.orientation_vertical -> {
+                    Toast.makeText(this, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                    hasChosenOrientation = true
+                    checkGenerateButton()
+                }
+            }
+            true
+
+        }
         popupMenu.show()
     }
 
