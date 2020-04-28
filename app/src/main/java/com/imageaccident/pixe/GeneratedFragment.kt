0ee5839825ebe5d038eee791160a4ea32664138e
tparry2 <tparry@mymail.mines.edu>
2020-04-27 21:36:05 -0600

    save and share

--- app/src/main/java/com/imageaccident/pixe/GeneratedFragment.kt
+++ app/src/main/java/com/imageaccident/pixe/GeneratedFragment.kt
@@ -1,10 +1,13 @@
 package com.imageaccident.pixe
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
+import android.os.Environment.getExternalStorageDirectory
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,12 +15,13 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
-import java.io.File
-import java.io.FileOutputStream
 import java.lang.Exception
 import java.util.*
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 
 
 class GeneratedFragment : Fragment() {
@@ -44,12 +48,15 @@ class GeneratedFragment : Fragment() {
             startActivity(Intent(requireContext(), MainActivity::class.java))
         }
 
-        shareButton.setOnClickListener {sendImage()}
+        shareButton.setOnClickListener {
+            Log.d(logTag, "In")
+            sendImage()
+        }
         saveButton.setOnClickListener {
 
             val result = saveImage(imageView.drawable.toBitmap())
             if (result) {
-                Toast.makeText(requireContext(), "Saved successfully!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Saved successfully! $imagePath", Toast.LENGTH_SHORT).show()
             }
             else {
                 Toast.makeText(requireContext(), "Error saving, try again.", Toast.LENGTH_SHORT).show()
@@ -65,11 +72,25 @@ class GeneratedFragment : Fragment() {
             return
         }
         try {
-            var sendPic = Intent(Intent.ACTION_SEND)
-            val path = "file://$imagePath"
-            sendPic.putExtra(Intent.EXTRA_STREAM, Uri.parse(path))
-            sendPic.setType("image/jpg")
-            startActivity(sendPic)
+            val sendPic = Intent(Intent.ACTION_SEND)
+            val path = "$imagePath"
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED) {
+                Log.d(logTag, "HERE")
+                sendPic.putExtra(Intent.EXTRA_STREAM, Uri.parse(path))
+                sendPic.setType("image/jpg")
+                Log.d(logTag, "THERE")
+                startActivity(sendPic)
+
+
+            } else {
+                 requestPermissions(requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                     Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_GRANTED)
+
+            }
+
         }catch (e: Exception) {
             e.printStackTrace()
             Toast.makeText(requireContext(), "Error sending image", Toast.LENGTH_SHORT).show()
@@ -77,34 +98,64 @@ class GeneratedFragment : Fragment() {
     }
 
     private fun saveImage(bitmap: Bitmap) : Boolean {
-        val root = Environment.getExternalStorageDirectory().toString()
-        val saveDir = File(root + "/pixe_photos/")
-        saveDir.mkdirs()
-
-        val gen = Random()
-        var temp = gen.nextInt(99999)
-        val filename = "img$temp.jpg"
-
-        val file = File(saveDir, filename)
-        if (file.exists()) {
-            file.delete()
-        }
+        val num = Random().nextInt(99999)
+        val imageName = "img$num.jpg"
+        val path = requireContext().getExternalFilesDir("image/*")
+        imagePath = "$path/DCIM/$imageName"
 
         try {
-            val output = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, output)
-            imagePath = file.absolutePath
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                MediaStore.Images.Media.insertImage(
+                    requireActivity().contentResolver,
+                    bitmap,
+                    imageName,
+                    "")
 
-            output.flush()
-            output.close()
+            } else {
+                requestPermissions(requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_GRANTED)
+
+            }
+
+
             return true
-        }
-        catch (e: Exception) {
+        }catch (e: Exception) {
             e.printStackTrace()
-            Log.d(logTag, e.stackTrace.toString())
             return false
         }
+
+
+//        val root = Environment.getExternalStorageDirectory().toString()
+//        val saveDir = File(root + "/pixe_photos/")
+//        saveDir.mkdirs()
+//
+
+//
+//        val file = File(saveDir, filename)
+//        if (file.exists()) {
+//            file.delete()
+//        }
+//
+//        try {
+//            val output = FileOutputStream(file)
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, output)
+//            imageName = file.absolutePath
+//
+//            output.flush()
+//            output.close()
+//            return true
+//        }
+//        catch (e: Exception) {
+//            e.printStackTrace()
+//            Log.d(logTag, e.stackTrace.toString())
+//            return false
+//        }
     }
+
+
 
 
 
