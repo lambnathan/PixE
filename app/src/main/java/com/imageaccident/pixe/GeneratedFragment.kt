5a04d76708d71d39e755e12d78f1dd9b843c8813
tparry2 <tparry@mymail.mines.edu>
2020-04-30 15:06:57 -0600

    saving and sharing

--- app/src/main/java/com/imageaccident/pixe/GeneratedFragment.kt
+++ app/src/main/java/com/imageaccident/pixe/GeneratedFragment.kt
@@ -1,13 +1,10 @@
 package com.imageaccident.pixe
 
-import android.content.ContentResolver
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import java.io.File
@@ -29,7 +27,9 @@ class GeneratedFragment(private val algorithm: String, private val orientation: 
     private lateinit var resetButton : Button
     private lateinit var shareButton : Button
     private lateinit var saveButton: Button
-    private var imagePath: String? = null
+
+    private lateinit var saveFile: File
+    private lateinit var saveUri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -84,48 +84,41 @@ class GeneratedFragment(private val algorithm: String, private val orientation: 
 }
 
     private fun sendImage() {
-        if(imagePath == null) {
+        if(saveUri == null) {
             Toast.makeText(requireContext(), "You must save the image before sending!", Toast.LENGTH_SHORT).show()
             return
         }
         try {
             var sendPic = Intent(Intent.ACTION_SEND)
-            val path = "file://$imagePath"
-            sendPic.putExtra(Intent.EXTRA_STREAM, Uri.parse(path))
+            sendPic.putExtra(Intent.EXTRA_STREAM, saveUri)
             sendPic.setType("image/jpg")
             startActivity(sendPic)
         }catch (e: Exception) {
             e.printStackTrace()
-            Toast.makeText(requireContext(), "Error sending image", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Error sending image.", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun saveImage(bitmap: Bitmap) : Boolean {
-        val root = Environment.getExternalStorageDirectory().toString()
-        val saveDir = File(root + "/pixe_photos/")
-        saveDir.mkdirs()
-
-        val gen = Random()
-        var temp = gen.nextInt(99999)
-        val filename = "img$temp.jpg"
-
-        val file = File(saveDir, filename)
-        if (file.exists()) {
-            file.delete()
-        }
-
         try {
-            val output = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, output)
-            imagePath = file.absolutePath
 
-            output.flush()
-            output.close()
+            val gen = Random()
+            var temp = gen.nextInt(99999)
+            val filename = "img$temp.jpg"
+
+            val dir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            saveFile = File.createTempFile(filename, ".jpg", dir)
+            saveUri = FileProvider.getUriForFile(
+                requireActivity(),
+                "com.imageaccident.pixe.fileprovider",
+                saveFile
+            )
+            val out = FileOutputStream(saveFile)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
             return true
         }
         catch (e: Exception) {
-            e.printStackTrace()
-            Log.d(logTag, e.stackTrace.toString())
+            Log.d(logTag, e.localizedMessage)
             return false
         }
     }
